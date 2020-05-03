a106f3b946388f3c3b51882bfff8ab802bf79efa
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2020-05-02 21:12:55 -0300

    add dummy main to test AWS services

---
+++ src/main/java/dpoi/serverless/scapping/Main.java
@@ -0,0 +1,24 @@
+package dpoi.serverless.scapping;
+
+import java.util.Random;
+
+public class Main {
+
+    private static int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+    public int handle() {
+        return getRandomNumberInRange(0, 100);
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
